b9241764b978893e97bebcc949dc31329c8f6fc0
Thiago Lima <thiagobenedictis@gmail.com>
2025-09-15 15:56:49 -0300

    Commit de teste 2

--- src/main/java/br/com/mariojp/figureeditor/App.java
+++ src/main/java/br/com/mariojp/figureeditor/App.java
@@ -5,6 +5,7 @@ import java.awt.*;
 
 public class App {
     public static void main(String[] args) {
+    	int x = 0;
         SwingUtilities.invokeLater(() -> {
             try {
                 UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
